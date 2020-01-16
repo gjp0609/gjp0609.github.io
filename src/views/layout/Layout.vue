96331f7f8849824d5381c3260832989f744de930
guojinpeng <gjp0609@qq.com>
2020-01-16 15:54:52 +0800

    edit layout

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,59 +1,62 @@
 <template>
-    <div>
-        <!--        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">-->
-        <!--            <el-radio-button :label="false">展开</el-radio-button>-->
-        <!--            <el-radio-button :label="true">收起</el-radio-button>-->
-        <!--        </el-radio-group>-->
-        <el-container>
-            <el-aside class="menu">
-                <el-menu
-                    @open="handleOpen"
-                    @close="handleClose"
-                    @select="handleSelect"
-                    :collapse="isCollapse"
-                    :unique-opened="true"
-                    :router="true"
-                    default-active="/"
-                >
-                    <template v-for="router in routerMap" v-if="!router.meta.hidden">
-                        <el-submenu v-if="router.children" :index="router.path">
-                            <template slot="title">
-                                <template v-if="router.meta.icon">
-                                    <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons">{{ router.meta.icon.name }}</i>
-                                    <i v-if="router.meta.icon.type === 'element-ui'" :class="router.meta.icon.name"></i>
-                                </template>
-                                <span slot="title">{{ router.meta.name }}</span>
-                            </template>
-                            <el-menu-item
-                                v-for="subRouter in router.children"
-                                v-if="!subRouter.meta.hidden"
-                                :key="subRouter.path"
-                                :index="router.path + '/' + subRouter.path"
-                            >
-                                <template v-if="subRouter.meta.icon">
-                                    <i v-if="subRouter.meta.icon.type === 'material-icons'" class="material-icons">{{ subRouter.meta.icon.name }}</i>
-                                    <i v-if="subRouter.meta.icon.type === 'element-ui'" :class="subRouter.meta.icon.name"></i>
-                                </template>
-                                <span> {{ subRouter.meta.name }}</span>
-                            </el-menu-item>
-                        </el-submenu>
-                        <el-menu-item v-else :index="router.path">
+    <div class="layout">
+        <aside :class="isCollapse ? 'is-collapse' : 'not-collapse'" class="menu">
+            <el-menu
+                @open="handleOpen"
+                @close="handleClose"
+                @select="handleSelect"
+                :collapse="isCollapse"
+                :unique-opened="true"
+                :router="true"
+                default-active="/"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+            >
+                <template v-for="router in routerMap" v-if="!router.meta.hidden">
+                    <el-submenu v-if="router.children" :index="router.path">
+                        <template slot="title">
                             <template v-if="router.meta.icon">
                                 <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons">{{ router.meta.icon.name }}</i>
                                 <i v-if="router.meta.icon.type === 'element-ui'" :class="router.meta.icon.name"></i>
                             </template>
-                            <span>{{ router.meta.name }}</span>
+                            <span slot="title">{{ router.meta.name }}</span>
+                        </template>
+                        <el-menu-item
+                            v-for="subRouter in router.children"
+                            v-if="!subRouter.meta.hidden"
+                            :key="subRouter.path"
+                            :index="router.path + '/' + subRouter.path"
+                        >
+                            <template v-if="subRouter.meta.icon">
+                                <i v-if="subRouter.meta.icon.type === 'material-icons'" class="material-icons">{{ subRouter.meta.icon.name }}</i>
+                                <i v-if="subRouter.meta.icon.type === 'element-ui'" :class="subRouter.meta.icon.name"></i>
+                            </template>
+                            <span> {{ subRouter.meta.name }}</span>
                         </el-menu-item>
-                    </template>
-                </el-menu>
-            </el-aside>
-            <el-container>
-                <el-header>666666</el-header>
-                <el-main>
-                    <router-view></router-view>
-                </el-main>
-            </el-container>
-        </el-container>
+                    </el-submenu>
+                    <el-menu-item v-else :index="router.path">
+                        <template v-if="router.meta.icon">
+                            <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons">{{ router.meta.icon.name }}</i>
+                            <i v-if="router.meta.icon.type === 'element-ui'" :class="router.meta.icon.name"></i>
+                        </template>
+                        <span>{{ router.meta.name }}</span>
+                    </el-menu-item>
+                </template>
+            </el-menu>
+        </aside>
+        <div class="main">
+            <header>
+                <i @click="isCollapse = !isCollapse" class="material-icons">dehaze</i>
+                <!--                <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">-->
+                <!--                    <el-radio-button :label="false">展开</el-radio-button>-->
+                <!--                    <el-radio-button :label="true">收起</el-radio-button>-->
+                <!--                </el-radio-group>-->
+            </header>
+            <section>
+                <router-view></router-view>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -86,10 +89,34 @@
 </script>
 
 <style lang="scss" scoped>
-    .menu {
-        width: 200px;
-        i {
-            font-size: 18px;
+    .layout {
+        display: flex;
+        flex-direction: row;
+        .menu {
+            &.is-collapse {
+                width: 65px;
+                /*transition-duration: 100ms;*/
+            }
+            &.not-collapse {
+                width: 300px;
+                /*transition-duration: 100ms;*/
+            }
+            i {
+                font-size: 18px;
+                text-align: center;
+                width: 20px;
+                &[class^='el-icon-'] {
+                    margin-right: 0;
+                }
+            }
+        }
+        .main {
+            width: 100%;
+            header {
+                height: 40px;
+                line-height: 40px;
+                background-color: lightgrey;
+            }
         }
     }
 </style>
