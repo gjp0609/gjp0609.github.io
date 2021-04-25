16f8c296bfd2e13a5ece2d197a4113eecb66acb4
GuoJinpeng <gjp0609@qq.com>
2021-04-25 17:40:31 +0800

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: './',
     build: {
         outDir: 'docs',
+        assetsDir: 'assets',
         emptyOutDir: false,
         chunkSizeWarningLimit: 1000
     },
