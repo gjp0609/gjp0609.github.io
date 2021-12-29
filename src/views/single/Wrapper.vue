a410ea0f88b50a05168bd247cf5bb7894e8c0df8
onysakura <gjp0609@qq.com>
2021-12-29 14:19:55 +0800

    update single wrapper

--- src/views/single/Wrapper.vue
+++ src/views/single/Wrapper.vue
@@ -3,7 +3,7 @@
         <div class="nav">
             Click to open in new tab: <a :href="url" target="_blank">{{ url }}</a>
         </div>
-        <iframe :src="url"></iframe>
+        <iframe class="shadow" :src="url"></iframe>
     </div>
 </template>
 
@@ -44,6 +44,46 @@
         flex: 1;
         .nav {
             margin-bottom: 1rem;
+            a {
+                color: unset;
+                text-decoration: none;
+            }
+        }
+        .shadow {
+            box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 20px rgba(0, 0, 0, 0.1) inset;
+        }
+        .shadow::before,
+        .shadow::after {
+            content: '';
+            position: absolute;
+            z-index: -1;
+        }
+        .shadow::before,
+        .shadow::after {
+            content: '';
+            position: absolute;
+            z-index: -1;
+            bottom: 15px;
+            left: 10px;
+            width: 50%;
+            height: 20%;
+        }
+        .shadow::before,
+        .shadow::after {
+            content: '';
+            position: absolute;
+            z-index: -1;
+            bottom: 15px;
+            left: 10px;
+            width: 50%;
+            height: 20%;
+            box-shadow: 0 15px 10px rgba(0, 0, 0, 0.7);
+            transform: rotate(-3deg);
+        }
+        .shadow::after {
+            right: 10px;
+            left: auto;
+            transform: rotate(3deg);
         }
         iframe {
             flex: 1;
