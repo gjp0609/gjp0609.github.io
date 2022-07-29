889bc27866388e5e7ea4161091832a89fd20b0e1
onysakura <gjp0609@qq.com>
2022-07-29 14:43:46 +0800

    change layout to ts

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="isDark ? 'dark' : 'light'" class="layout">
+    <div :class="isDark ? 'dark' : 'light'" class="root-layout">
         <aside :class="isCollapse ? 'is-collapse' : 'not-collapse'" class="menu">
             <el-menu
                 :mode="isMobile ? 'horizontal' : 'vertical'"
@@ -9,43 +9,41 @@
                 :router="true"
                 :unique-opened="true"
             >
-                <template v-for="router in routerMap">
-                    <template v-if="!router.meta.hidden">
-                        <el-sub-menu v-if="router.children" :index="router.path">
-                            <template #title>
-                                <template v-if="router.meta.icon">
-                                    <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons outlined">
-                                        {{ router.meta.icon.name }}
-                                    </i>
-                                    <el-icon v-if="router.meta.icon.type === 'element-ui'">
-                                        <component :is="router.meta.icon.name"></component>
-                                    </el-icon>
-                                </template>
-                                <span>{{ router.meta.name }}</span>
-                            </template>
-                            <el-menu-item v-for="subRouter in router.children" :key="subRouter.path" :index="router.path + '/' + subRouter.path">
-                                <template v-if="!subRouter.meta.hidden">
-                                    <template v-if="subRouter.meta.icon">
-                                        <i v-if="subRouter.meta.icon.type === 'material-icons'" class="material-icons outlined">
-                                            {{ subRouter.meta.icon.name }}
-                                        </i>
-                                        <el-icon v-if="subRouter.meta.icon.type === 'element-ui'">
-                                            <component :is="subRouter.meta.icon.name"></component>
-                                        </el-icon>
-                                    </template>
-                                    <span> {{ subRouter.meta.name }}</span>
-                                </template>
-                            </el-menu-item>
-                        </el-sub-menu>
-                        <el-menu-item v-else :index="router.path">
+                <template v-for="router in routerMap" :key="router.path">
+                    <el-sub-menu v-if="router.children" :index="router.path">
+                        <template #title>
                             <template v-if="router.meta.icon">
-                                <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons outlined">{{ router.meta.icon.name }}</i>
-                                <i v-if="router.meta.icon.type === 'element-ui'" :class="router.meta.icon.name"></i>
-                                <component :is="router.meta.icon.name" v-if="router.meta.icon.type === 'element-plus'" />
+                                <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons outlined">
+                                    {{ router.meta.icon.name }}
+                                </i>
+                                <el-icon v-if="router.meta.icon.type === 'element-ui'">
+                                    <component :is="router.meta.icon.name"></component>
+                                </el-icon>
                             </template>
                             <span>{{ router.meta.name }}</span>
+                        </template>
+                        <el-menu-item v-for="subRouter in router.children" :key="subRouter.path" :index="router.path + '/' + subRouter.path">
+                            <template v-if="!subRouter.meta.hidden">
+                                <template v-if="subRouter.meta.icon">
+                                    <i v-if="subRouter.meta.icon.type === 'material-icons'" class="material-icons outlined">
+                                        {{ subRouter.meta.icon.name }}
+                                    </i>
+                                    <el-icon v-if="subRouter.meta.icon.type === 'element-ui'">
+                                        <component :is="subRouter.meta.icon.name"></component>
+                                    </el-icon>
+                                </template>
+                                <span> {{ subRouter.meta.name }}</span>
+                            </template>
                         </el-menu-item>
-                    </template>
+                    </el-sub-menu>
+                    <el-menu-item v-else :index="router.path">
+                        <template v-if="router.meta.icon">
+                            <i v-if="router.meta.icon.type === 'material-icons'" class="material-icons outlined">{{ router.meta.icon.name }}</i>
+                            <i v-if="router.meta.icon.type === 'element-ui'" :class="router.meta.icon.name"></i>
+                            <component :is="router.meta.icon.name" v-if="router.meta.icon.type === 'element-plus'" />
+                        </template>
+                        <span>{{ router.meta.name }}</span>
+                    </el-menu-item>
                 </template>
             </el-menu>
             <div v-if="!isMobile" class="operations">
@@ -65,78 +63,68 @@
     </div>
 </template>
 
-<script>
+<script lang="ts" setup>
     import { constantMenuRouterMap } from '../../router/index';
     import moment from 'moment';
+    import { ref, onMounted, watch, computed } from 'vue';
+    import { useRouter } from 'vue-router';
 
-    export default {
-        name: 'Layout',
-        data() {
-            return {
-                screenWidth: document.body.clientWidth,
-                isCollapse: localStorage.getItem('menuIsCollapse') === '1' || false,
-                routerMap: constantMenuRouterMap,
-                defaultActive: '/',
-                isDark: localStorage.getItem('themeIsDark') === '1' || false,
-                time: ''
-            };
-        },
-        computed: {
-            isMobile() {
-                return this.screenWidth < 1024;
-            }
-        },
-        watch: {
-            isDark(newVal) {
-                console.log(newVal);
-                console.log(document);
-                document.documentElement.className = newVal ? 'dark' : '';
-            }
-        },
-        mounted() {
-            let indexPath = '/index/home';
-            console.log(this.$router.currentRoute.value.fullPath);
-            if (this.$router.currentRoute.value.fullPath !== indexPath) {
-                this.defaultActive = this.$router.currentRoute.value.fullPath;
-            }
-            this.$router.beforeEach((to, from, next) => {
-                this.defaultActive = to.path;
-                if (to.path === indexPath) {
-                    this.defaultActive = '/';
-                }
-                next();
-            });
-            setInterval(() => {
-                this.time = moment().format('YYYY-MM-DD HH:mm:ss');
-            }, 500);
-            window.onresize = () => {
-                return (() => {
-                    window.screenWidth = document.body.clientWidth;
-                    _this.screenWidth = window.screenWidth;
-                    console.log(_this.screenWidth);
-                })();
-            };
-            this.isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-                this.isDark = event.matches;
-            });
-        },
-        methods: {
-            menuCollapseChange() {
-                this.isCollapse = !this.isCollapse;
-                localStorage.setItem('menuIsCollapse', this.isCollapse ? '1' : '0');
-            },
-            handleThemeChange() {
-                this.isDark = !this.isDark;
-                localStorage.setItem('themeIsDark', this.isDark ? '1' : '0');
-            },
-            async copy() {
-                let blob = new Blob([this.time], { type: 'text/plain' });
-                let item = new ClipboardItem({ 'text/plain': blob });
-                navigator.clipboard.write([item]);
-            }
+    const $router = useRouter();
+    const screenWidth = ref(document.body.clientWidth);
+    const isCollapse = ref(localStorage.getItem('menuIsCollapse') === '1' || false);
+    const routerMap = ref(constantMenuRouterMap);
+    const defaultActive = ref('/');
+    const isDark = ref(localStorage.getItem('themeIsDark') === '1' || false);
+    const time = ref('');
+
+    onMounted(() => {
+        let indexPath = '/index/home';
+        console.log($router.currentRoute.value.fullPath);
+        if ($router.currentRoute.value.fullPath !== indexPath) {
+            defaultActive.value = $router.currentRoute.value.fullPath;
         }
-    };
+        $router.beforeEach((to, from, next) => {
+            defaultActive.value = to.path;
+            if (to.path === indexPath) {
+                defaultActive.value = '/';
+            }
+            next();
+        });
+        setInterval(() => {
+            time.value = moment().format('YYYY-MM-DD HH:mm:ss');
+        }, 500);
+        window.onresize = () => {
+            return (() => {
+                screenWidth.value = document.body.clientWidth;
+            })();
+        };
+        isDark.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+            isDark.value = event.matches;
+        });
+    });
+
+    const isMobile = computed(() => screenWidth.value < 1024);
+
+    watch(isDark, (value) => {
+        document.documentElement.className = value ? 'dark' : '';
+    });
+
+    function menuCollapseChange() {
+        isCollapse.value = !isCollapse.value;
+        localStorage.setItem('menuIsCollapse', isCollapse.value ? '1' : '0');
+    }
+
+    function handleThemeChange() {
+        isDark.value = !isDark.value;
+        localStorage.setItem('themeIsDark', isDark.value ? '1' : '0');
+    }
+
+    async function copy() {
+        let blob = new Blob([time.value], { type: 'text/plain' });
+        let item = new ClipboardItem({ 'text/plain': blob });
+        await navigator.clipboard.write([item]);
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -153,7 +141,7 @@
             }
         }
     }
-    .layout {
+    .root-layout {
         height: 100vh;
         display: flex;
         overflow: hidden;
@@ -165,14 +153,14 @@
         }
         .menu {
             display: flex;
-            border-right: 0.01rem solid var(--el-border-color);
+            .el-menu {
+                border: none;
+            }
             @media screen and (min-width: 1024px) {
+                border-right: 0.01rem solid var(--el-border-color);
                 $menuCollapseWidth: 65px;
                 flex-direction: column;
                 justify-content: space-between;
-                .el-menu {
-                    border: none;
-                }
                 &.is-collapse {
                     width: $menuCollapseWidth;
                     transition-duration: 300ms;
@@ -199,9 +187,10 @@
                 }
             }
             @media screen and (max-width: 1024px) {
+                border-bottom: 0.01rem solid var(--el-border-color);
                 $menuHeight: 2.5rem;
                 .el-menu {
-                    width: 100vh;
+                    max-width: 100vw;
                     display: flex;
                     flex-direction: row;
                     justify-content: space-around;
@@ -220,6 +209,20 @@
             flex: 1;
             display: flex;
             flex-direction: column;
+            height: 100vh;
+            header {
+                $headerHeight: 2.4rem;
+                height: $headerHeight;
+                line-height: $headerHeight;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                padding: 0 0.5rem;
+                border-bottom: 0.01rem solid var(--el-border-color);
+                .time {
+                    margin: 0;
+                }
+            }
             section {
                 flex: 1;
                 display: flex;
@@ -228,20 +231,7 @@
                 @media screen and (max-width: 1024px) {
                     padding: 0.5rem;
                 }
-            }
-            @media screen and (min-width: 1024px) {
-                header {
-                    $headerHeight: 2.4rem;
-                    height: $headerHeight;
-                    line-height: $headerHeight;
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: space-between;
-                    padding: 0 0.5rem;
-                    .time {
-                        margin: 0;
-                    }
-                }
+                overflow-y: auto;
             }
         }
     }
